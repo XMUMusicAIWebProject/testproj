0f80252fd5108315b8cc16db9db66fece102404f
Lin Geng <20420162201666@stu.xmu.edu.cn>
2018-12-05 19:22:45 +0800

    Add Entity
    add Grade

---
+++ src/main/java/com/sys3/model/Grade.java
@@ -0,0 +1,25 @@
+package com.sys3.model;
+
+public class Grade {
+    private Integer studentId;
+    private Integer round;
+
+    public Grade() {
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public Integer getRound() {
+        return round;
+    }
+
+    public void setRound(Integer round) {
+        this.round = round;
+    }
+}
