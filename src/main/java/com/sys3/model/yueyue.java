00592ee6e89d9aeebb90b5f00677b12b72ca54e0
Nikawool <491158566@qq.com>
2018-12-05 20:08:35 +0800

    add entity
    1.add location
    2.add model

---
+++ src/main/java/com/sys3/model/yueyue.java
@@ -0,0 +1,12 @@
+package com.sys3.model;
+
+public class yueyue {
+    private static yueyue ourInstance = new yueyue();
+
+    public static yueyue getInstance() {
+        return ourInstance;
+    }
+
+    private yueyue() {
+    }
+}
